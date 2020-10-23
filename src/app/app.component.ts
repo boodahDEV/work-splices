a1fa6cd79882b0eea3718490402cca2c6b943858
boodah <boodah21@protonmail.com>
2020-10-22 22:29:00 -0500

    Ajustes de responsive al archivo app principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,8 +11,15 @@ export class AppComponent {
   uid: string
   items = [
     { title: 'Configurar', icon: 'settings-2-outline' },
-    { title: 'Salir',  icon: 'log-out-outline', },
+    { title: 'Salir', icon: 'log-out-outline', },
   ];
 
   title = 'work-splice';
+  is_login:boolean;
+  constructor() {
+    if(!sessionStorage.getItem("token-s"))
+      this.is_login = false
+    else
+      this.is_login = true
+  }
 }
