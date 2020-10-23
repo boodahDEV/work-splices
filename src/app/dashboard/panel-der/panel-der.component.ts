4ccab06b7b097f8e9c8dd405523ebf4d00595fc1
boodah <boodah21@protonmail.com>
2020-10-22 22:41:13 -0500

    Se crean las primicias de los componentes para el panel derecho.

--- src/app/dashboard/panel-der/panel-der.component.ts
+++ src/app/dashboard/panel-der/panel-der.component.ts
@@ -6,7 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./panel-der.component.scss']
 })
 export class PanelDerComponent implements OnInit {
-
+  users: { name: string, title: string }[] = [
+    { name: 'Proyecto 1', title: 'Nurse' },
+    { name: 'Proyecto 2', title: 'Doctor of Medicine' },
+    { name: 'Janitor', title: 'Janitor' },
+    { name: 'Perry Cox', title: 'Doctor of Medicine' },
+    { name: 'Ben Sullivan', title: 'Carpenter and photographer' },
+    { name: 'Ben Sullivan', title: 'Carpenter and photographer' },
+    { name: 'Ben Sullivan', title: 'Carpenter and photographer' },
+  ];
   constructor() { }
 
   ngOnInit(): void {
