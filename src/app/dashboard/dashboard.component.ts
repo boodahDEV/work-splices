1b74fd8f9b198d34c8831927c1498c83c5716d43
boodah <boodah21@protonmail.com>
2020-10-22 22:29:47 -0500

    ajustes al grid principal del dashboard.

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  is_login = true;
+  i = [1,2,3,4,54]
 
   constructor() { }
 
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -4,16 +4,29 @@ import { DashboardComponent } from './dashboard.component';
 import { PanelIzqComponent } from './panel-izq/panel-izq.component';
 import { PanelCentralComponent } from './panel-central/panel-central.component';
 import { PanelDerComponent } from './panel-der/panel-der.component';
+import { NbActionsModule, NbAlertModule, NbBadgeModule, NbButtonModule, NbCardModule, NbIconModule, NbLayoutModule, NbListModule, NbRadioModule, NbTooltipModule, NbUserModule } from '@nebular/theme';
+import { PanelSurComponent } from './panel-sur/panel-sur.component';
 
 @NgModule({
     declarations: [
       DashboardComponent,
       PanelIzqComponent,
       PanelCentralComponent,
-      PanelDerComponent
+      PanelDerComponent,
+      PanelSurComponent
     ],
     imports: [
       BrowserModule,
+      NbLayoutModule,
+      NbButtonModule,
+      NbCardModule,
+      NbBadgeModule,
+      NbListModule,
+      NbTooltipModule,
+      NbIconModule,
+      NbUserModule,
+      NbRadioModule,
+      NbActionsModule
     ],
     providers: [],
   })
